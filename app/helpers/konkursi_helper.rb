e098167c3e49d0864035dfd305b19ee1151fd461
sekulic <sekulic.radosav@yahoo.com>
2014-10-29 23:59:22 +0100

    refactor konkursi controlle2r

--- app/helpers/konkursi_helper.rb
+++ app/helpers/konkursi_helper.rb
@@ -1,2 +1,11 @@
 module KonkursiHelper
+ def konkursi_filter(status_ids, konkursi_vrsta)
+   @svi_konkursi_filter = Array.new
+   status_ids.each do |status_id|
+    konkursi_vrsta.each do |konkurs|
+      @svi_konkursi_filter.push(konkurs) if konkurs.status_id == status_id.to_i
+    end    
+   end
+    return @svi_konkursi_filter   
+ end      
 end
