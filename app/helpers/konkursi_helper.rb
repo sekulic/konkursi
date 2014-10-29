a6cc2cac39dd5cde16027a49a8c34a86cf59d33b
sekulic <sekulic.radosav@yahoo.com>
2014-10-30 00:52:00 +0100

    refactor konkursi controller3.3

--- app/helpers/konkursi_helper.rb
+++ app/helpers/konkursi_helper.rb
@@ -1,9 +1,9 @@
 module KonkursiHelper
- def konkursi_filter(ids, konkursi, @atribut)
+ def konkursi_filter(ids, konkursi, atribut)
    @svi_konkursi_filter = Array.new
    ids.each do |id|
     konkursi.each do |konkurs|
-      @svi_konkursi_filter.push(konkurs) if konkurs.@atribut == status_id.to_i
+      @svi_konkursi_filter.push(konkurs) if konkurs.atribut == status_id.to_i
     end    
    end
     return @svi_konkursi_filter   
