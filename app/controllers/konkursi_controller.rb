c546aba383b8d229930c1da08d82ac6f5082c40b
sekulic <sekulic.radosav@yahoo.com>
2014-10-31 21:47:19 +0100

    refactor konkursi controller8.4

--- app/controllers/konkursi_controller.rb
+++ app/controllers/konkursi_controller.rb
@@ -12,9 +12,9 @@ class KonkursiController < ApplicationController
   def search
     if params[:konkurs]
        if konkurs.has_key?(:vrsta_ids)
-       @konkursi_vrsta = Konkurs.find_all_by_vrsta_id(konkurs[:vrsta_ids]).map{ |x| x.id }
+       @konkursi_vrsta = Konkurs.find_all_by_vrsta_id(konkurs[:vrsta_ids])
        else
-       @konkursi_vrsta = Konkurs.all.map{ |x| x.id }  
+       @konkursi_vrsta = Konkurs.all
        end 
        @konkursi = Array.new
        @konkursi = @konkursi_vrsta
