41671ae15749fbb0f01195bf8d16d1b9719bbfcd
sekulic <sekulic.radosav@yahoo.com>
2015-03-04 20:38:16 +0100

    change some css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Projekti::Application.configure do
   }
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'http://www.projekti.in.rs' }
+  config.action_mailer.default_url_options = { host: 'www.projekti.in.rs' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
